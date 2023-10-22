69395907d842c89257d5ad021c4a97d2125970f8
Robert B. Fesko <fesko.robert@gmail.com>
2023-10-22 20:48:38 +0000

    update iteration 2

--- TheMiddleman/Program.cs
+++ TheMiddleman/Program.cs
@@ -22,20 +22,10 @@ class Program
         return Console.ReadLine() ?? "";
     }
 
-    static Middleman CreateMiddleman(int index)
-    {
-        string middlemanName = GetMiddlemanName(index);
-        string companyName = GetCompany(middlemanName);
-        int initialBalance = GetInitialBalance();
-
-        return new Middleman(middlemanName, companyName, initialBalance);
-    }
-
     static int GetInitialBalance()
     {
         Console.WriteLine("Schwierigkeitsgrad auswählen (Einfach, Normal, Schwer):");
         string difficulty = Console.ReadLine()?.ToLower() ?? "normal";
-
         switch (difficulty)
         {
             case "einfach":
@@ -49,13 +39,21 @@ class Program
         }
     }
 
-    static List<Middleman> InitializeMiddlemen()
+    static Middleman InitializeSingleMiddleman(int index)
     {
-        List<Middleman> middlemen = new List<Middleman>();
+        string middlemanName = GetMiddlemanName(index);
+        string companyName = GetCompany(middlemanName);
+        int initialBalance = GetInitialBalance();
+        return new Middleman(middlemanName, companyName, initialBalance);
+    }
+
+    static List<Middleman> InitializeAllMiddlemen()
+    {
         int amountOfMiddlemen = GetAmountOfMiddlemen();
+        List<Middleman> middlemen = new List<Middleman>();
         for (int i = 1; i <= amountOfMiddlemen; i++)
         {
-            middlemen.Add(CreateMiddleman(i));
+            middlemen.Add(InitializeSingleMiddleman(i));
         }
         return middlemen;
     }
@@ -64,7 +62,6 @@ class Program
     {
         Console.WriteLine($"{middleman.Name} von {middleman.Company} | ${middleman.AccountBalance} | Tag {currentDay}");
     }
-
 
     static string ReadProductName(string line)
     {
@@ -92,7 +89,6 @@ class Program
         List<Product> products = new List<Product>();
         Product? currentProduct = null;
         int idCounter = 1;
-
         foreach (var line in lines)
         {
             if (line.StartsWith("- name: "))
@@ -118,39 +114,39 @@ class Program
                 }
             }
         }
-
         return products;
     }
 
     static void ShowMenuAndTakeAction(Middleman middleman, ref int currentDay, List<Product> products)
     {
         bool endRound = false;
-
         while (!endRound)
         {
             DisplayMiddlemanInfo(middleman, currentDay);
             Console.WriteLine("e) Einkaufen");
             Console.WriteLine("v) Verkaufen");
             Console.WriteLine("b) Runde beenden");
-
             string userChoice = Console.ReadLine() ?? "";
+            HandleUserChoice(userChoice, middleman, products, ref endRound);
+        }
+    }
 
-            switch (userChoice)
-            {
-                case "b":
-                    endRound = true;
-                    break;
-                case "e":
-                    ShowShoppingMenu(products, middleman);
-                    endRound = false;
-                    break;
-                case "v":
-                    ShowSellingMenu(middleman);
-                    break;
-                default:
-                    Console.WriteLine("Ungültige Auswahl. Bitte erneut versuchen.");
-                    break;
-            }
+    static void HandleUserChoice(string choice, Middleman middleman, List<Product> products, ref bool endRound)
+    {
+        switch (choice)
+        {
+            case "b":
+                endRound = true;
+                break;
+            case "e":
+                ShowShoppingMenu(products, middleman);
+                break;
+            case "v":
+                ShowSellingMenu(middleman);
+                break;
+            default:
+                Console.WriteLine("Ungültige Auswahl. Bitte erneut versuchen.");
+                break;
         }
     }
 
@@ -162,53 +158,42 @@ class Program
             Console.WriteLine($"{product.Id} {product.Name} ({product.Durability} Tage) ${product.BasePrice}/Stück");
         }
         Console.WriteLine("z) Zurück");
-
         string? userChoice = Console.ReadLine();
-
         if (userChoice == "z")
         {
             return;
         }
+        SelectProductAndPurchase(middleman, userChoice, products);
+    }
 
+    static void SelectProductAndPurchase(Middleman middleman ,string? userChoice, List<Product> products)
+    {
         int selectedProductId;
-
         if (!int.TryParse(userChoice, out selectedProductId) || int.Parse(userChoice) <= 0)
         {
             return;
         }
-
         Product selectedProduct = products.Find(p => p.Id == selectedProductId);
-
         Console.WriteLine($"Wieviel von {selectedProduct.Name} kaufen?");
         int quantity = int.Parse(Console.ReadLine());
-
         if (quantity <= 0)
         {
             return;
         }
+        ExecutePurchase(middleman, selectedProduct, quantity);
+    }
 
+    static void ExecutePurchase(Middleman middleman, Product selectedProduct, int quantity)
+    {
         int totalCost = quantity * selectedProduct.BasePrice;
-
         if (middleman.AccountBalance < totalCost)
         {
             Console.WriteLine("Nicht genügend Geld vorhanden.");
             return;
         }
-        else
-        {
-            middleman.AccountBalance -= totalCost;
-
-            if (middleman.OwnedProducts.ContainsKey(selectedProduct))
-            {
-                middleman.OwnedProducts[selectedProduct] += quantity;
-            }
-            else
-            {
-                middleman.OwnedProducts.Add(selectedProduct, quantity);
-            }
-
-            Console.WriteLine($"Kauf erfolgreich. Neuer Kontostand: ${middleman.AccountBalance}");
-        }
+        middleman.AccountBalance -= totalCost;
+        middleman.OwnedProducts.Add(selectedProduct, quantity);
+        Console.WriteLine($"Kauf erfolgreich. Neuer Kontostand: ${middleman.AccountBalance}");
     }
 
     static void ShowSellingMenu(Middleman middleman)
@@ -221,35 +206,33 @@ class Program
             index++;
         }
         Console.WriteLine("z) Zurück");
-
         string userChoice = Console.ReadLine() ?? "";
         if (userChoice == "z")
         {
             return;
         }
-
         int selectedProductIndex = int.Parse(userChoice);
         var selectedEntry = middleman.OwnedProducts.ElementAt(selectedProductIndex - 1);
         var selectedProduct = selectedEntry.Key;
         var availableQuantity = selectedEntry.Value;
-
         Console.WriteLine($"Wieviel von {selectedProduct.Name} verkaufen (max. {availableQuantity})?");
         int quantityToSell = int.Parse(Console.ReadLine() ?? "0");
-
         if (quantityToSell <= 0 || quantityToSell > availableQuantity)
         {
             Console.WriteLine("Ungültige Menge.");
             return;
         }
+        ExecuteSale(middleman, selectedProduct, quantityToSell);
+    }
 
+    static void ExecuteSale(Middleman middleman, Product selectedProduct, int quantityToSell)
+    {
         middleman.AccountBalance += quantityToSell * selectedProduct.SellingPrice;
         middleman.OwnedProducts[selectedProduct] -= quantityToSell;
-
         if (middleman.OwnedProducts[selectedProduct] == 0)
         {
             middleman.OwnedProducts.Remove(selectedProduct);
         }
-
         Console.WriteLine($"Verkauf erfolgreich. Neuer Kontostand: ${middleman.AccountBalance}");
     }
 
@@ -269,18 +252,15 @@ class Program
         {
             ShowMenuAndTakeAction(middleman, ref currentDay, products);
         }
-
         RotateMiddlemen(middlemen);
-
         currentDay++;
     }
 
     static void Main()
     {
-        List<Middleman> middlemen = InitializeMiddlemen();
+        List<Middleman> middlemen = InitializeAllMiddlemen();
         List<Product> products = ReadProducts();
         int currentDay = 1;
-
         while (true)
         {
             SimulateDay(middlemen, ref currentDay, products);
--- TheMiddleman/entity/Middleman.cs
+++ TheMiddleman/entity/Middleman.cs
@@ -10,9 +10,9 @@ namespace TheMiddleman.Entity
             Company = company;
             AccountBalance = accountBalance;
         }
-        
-        public string? Name { get; set; }
-        public string? Company { get; set; }
+
+        public string? Name { get; }
+        public string? Company { get; }
         public int AccountBalance { get; set; }
         public Dictionary<Product, int> OwnedProducts { get; set; } = new Dictionary<Product, int>();
     }
