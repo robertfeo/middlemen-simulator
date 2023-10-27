a76f81bc50da7a511bd1b3d760665d6226f461c0
Robert B. Fesko <fesko.robert@gmail.com>
2023-10-27 17:06:53 +0200

    Update Product.cs

--- TheMiddleman/entity/Product.cs
+++ TheMiddleman/entity/Product.cs
@@ -1,12 +1,11 @@
 public class Product
 {
     public int Id { get; set; }
-
     public required string Name { get; set; }
-
     public int Durability { get; set; }
-
     public int BasePrice { get; set; }
-
     public int SellingPrice => (int)Math.Round(BasePrice * 0.8);
+    public int MinProductionRate { get; set; }
+    public int MaxProductionRate { get; set; }
+    public int AvailableQuantity { get; set; } = 0;
 }
