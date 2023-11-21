42922ff9e208dcc38af4b34e2e89745378d10c22
Robert B. Fesko <fesko.robert@gmail.com>
2023-11-21 13:22:10 +0000

    update

--- TheMiddleman/Entities/Product.cs
+++ TheMiddleman/Entities/Product.cs
@@ -5,7 +5,7 @@ public class Product
     public int Durability { get; set; }
     public int BasePrice { get; set; }
     public int PurchasePrice { get; set; }
-    public int SellingPrice => (int)Math.Round(BasePrice * 0.8);
+    public int SellingPrice => (int) Math.Round(BasePrice * 0.8);
     public int MinProductionRate { get; set; }
     public int MaxProductionRate { get; set; }
     public int AvailableQuantity { get; set; } = 0;
