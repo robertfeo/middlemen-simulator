718752f66cb46fc0c2996ce17eea1578217e1b31
robertfeo <fesko.robert@gmail.com>
2023-12-11 20:03:38 +0100

    update

--- TheMiddleman/obj/Debug/net8.0/TheMiddleman.AssemblyInfo.cs
+++ TheMiddleman/obj/Debug/net8.0/TheMiddleman.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheMiddleman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48d7cfd506722ef6094531b857dd28194573007c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheMiddleman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheMiddleman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
